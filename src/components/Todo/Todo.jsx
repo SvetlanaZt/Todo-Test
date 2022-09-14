7dcb582d588844142057145a636bceb72803b4ae
SvetlanaZt <svetlana.zt23@gmail.com>
2022-09-14 15:33:00 +0300

    3333

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -9,20 +9,25 @@ import css from './Todo.module.css'
 export default function Todo() {
     const [data, setData] = useState([]);
     const [filter, setFilter] = useState('');
+    const [dataLocal, setDataLocal] = useState([]);
+    console.log(dataLocal)
 
     useEffect(() => {
-        // const savedSettings = localStorage.getItem("settings");
-        // const parsedSettings = JSON.parse(savedSettings);
-        fetchData().then(data => setData([...data.filter(a=>!a.completed), 
+        const savedSettings = localStorage.getItem("settings");
+        const parsedSettings = JSON.parse(savedSettings);
+    
+        fetchData().then(data => setData([...(parsedSettings || []), ...data.filter(a=>!a.completed), 
             ...data.filter(a=>a.completed)]))
       }, [])
     
       const takeName = (contacts)=> {
         console.log(contacts)
-        localStorage.setItem("settings", JSON.stringify({id: nanoid(), title: contacts, completed: false }));
+        setDataLocal([{id: nanoid(), title: contacts, completed: false },...dataLocal])
+        localStorage.setItem("settings", JSON.stringify([{id: nanoid(), title: contacts, completed: false },...dataLocal]));
         const savedSettings = localStorage.getItem("settings");
         const parsedSettings = JSON.parse(savedSettings);
-        setData(prevState => [parsedSettings, ...prevState])
+        
+        setData(prevState => [...parsedSettings, ...prevState])
       }
     
   const changeInput = (evt) => { 
@@ -63,18 +68,3 @@ export default function Todo() {
 
     )
 }
-
-
-{/* <section className={css.section}>
-<div className={css.headerTodo}></div>
-<h1 className={css.titleTodo}>Todo App</h1>
-<div className={css.sectionTodo}>
-<section className={css.sectionTo}>
-    <AddTodo onClick={takeName}/>
-    </section>
-<section className={css.sectionMyTodo}>
-    <Fillter value={filter} onChange={changeInput} />
-    <MyTodo value={onChangeInput} onTakeBoole={chanheBoole}/>
-    </section>
-</div>
-</section> */}
