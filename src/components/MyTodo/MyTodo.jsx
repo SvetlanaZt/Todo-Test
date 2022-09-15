3804ab41102645b68a374d10c9ff2444e2ba7044
SvetlanaZt <svetlana.zt23@gmail.com>
2022-09-15 09:39:26 +0300

    fin

--- src/components/MyTodo/MyTodo.jsx
+++ src/components/MyTodo/MyTodo.jsx
@@ -1,18 +1,50 @@
-// import { useState} from 'react';
+import { useState, useEffect} from 'react';
 import css from './MyTodo.module.css'
 
 
 export default function MyTodo({value, onTakeBoole}) {
-    return(
-        <div>
-             <div className={css.myTodoComplited}>
-            {/* <p onClick={()=>allTodo(contacts)} className={css.myTodoComplitedItem}>Все /</p> */}
-             {/* <p onClick={()=>value.map(item=>console.log(item.completed))} className={css.myTodoComplitedItem}>Выполнены /</p> */}
-             {/* <p onClick={()=>value.filter(item=>item.completed)} className={css.myTodoComplitedItem}>Не выполнены</p> */}
-             </div>
-
+    const [visibleToDoes, setVisibleToDoes] = useState([]);
+    const [viewByComplited, setViewByComplited] = useState('all');
+  
+    useEffect(() => {
+      setVisibleToDoes(
+        value.filter(toDo => {
+          switch (viewByComplited) {
+            case 'complited':
+              return toDo.completed;
+            case 'notComplited':
+              return !toDo.completed;
+            default:
+              return true;
+          }
+        })
+      );
+    }, [value, viewByComplited]);
+  
+    return (
+      <div>
+        <div className={css.myTodoComplited}>
+          <p
+            onClick={() => setViewByComplited('all')}
+            className={css.myTodoComplitedItem}
+          >
+            Все /
+          </p>
+          <p
+            onClick={() => setViewByComplited('complited')}
+            className={css.myTodoComplitedItem}
+          >
+            Выполнены /
+          </p>
+          <p
+            onClick={() => setViewByComplited('notComplited')}
+            className={css.myTodoComplitedItem}
+          >
+            Не выполнены
+          </p>
+        </div>
         <ul className={css.myTodoList}>
-        {value.map(item => (
+        {visibleToDoes.map(item => (
             <li key={item.id} className={css.item}>{item.title} 
             <div className={item.completed ? css.onButton : css.offButton}>
             <input type='checkbox' onClick={() => onTakeBoole(item)} className={css.checkbox}/></div></li>
@@ -22,3 +54,12 @@ export default function MyTodo({value, onTakeBoole}) {
      </div>
     )
 }
+
+
+
+{/* <div>
+<div className={css.myTodoComplited}>
+<p onClick={()=>onClickFillter('All')} className={css.myTodoComplitedItem}>Все /</p>
+<p onClick={()=>onClickFillter('Done')} className={css.myTodoComplitedItem}>Выполнены /</p>
+<p onClick={()=>onClickFillter('False')} className={css.myTodoComplitedItem}>Не выполнены</p>
+</div> */}
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -9,6 +9,7 @@ import css from './Todo.module.css'
 export default function Todo() {
     const [data, setData] = useState([]);
     const [filter, setFilter] = useState('');
+    const [filteredToDoes, setFilteredToDoes] = useState([]);
     const [dataLocal] = useState([]);
     console.log(dataLocal)
 
@@ -20,7 +21,15 @@ export default function Todo() {
             ...data.filter(a=>a.completed)]))
       }, [])
 
-      const takeName = (contacts)=> {
+      useEffect(() => {
+        setFilteredToDoes(
+          data.filter(toDo =>
+            toDo.title.toLowerCase().includes(filter.toLowerCase())
+          )
+        );
+      }, [filter, data]);
+
+      const addName = (contacts)=> {
         const savedSettings = localStorage.getItem("settings");
         const parsedSettings = JSON.parse(savedSettings) || [];
         const newItem = {id: nanoid(), title: contacts, completed: false };
@@ -35,20 +44,35 @@ export default function Todo() {
   }
 
   const chanheBoole = (task) => {
-    // const updatedData = contacts.map(item => {
-    //   return item.id === task.id ? {
-    //     ...item,
-    //     completed: !item.completed
-    //   } : {
-    //     ...item
-    //   }
-    // });
-    // setContacts(updatedData)
+    const updatedData = data.map(item => {
+      return item.id === task.id ? {
+        ...item,
+        completed: !item.completed
+      } : {
+        ...item
+      }
+    });
+    setData(updatedData)
   }
 
-  const onChangeInput = data && data.filter(item => item.title.toLowerCase().includes(filter.toLowerCase()));
-  // const tfbfbg = onChangeInput.filter(item => item.completed)
+  // const onChangeInput = data && data.filter(item => item.title.toLowerCase().includes(filter.toLowerCase()));
+  
 
+// const onClickFillter = (e)=>{
+// console.log(e)
+// switch (e) {
+//   case 'All':
+//     setData(prevState=> prevState.filter(item => item.title.toLowerCase().includes(filter.toLowerCase())));
+//     break;
+//   case 'Done':
+//     setData(prevState=> prevState.filter(i=>i.completed));
+//     break; 
+//    case 'False':
+//     setData(prevState=> prevState.filter(i=>!i.completed));
+//     break; 
+// default: return;
+// }
+// }
 
     return(
  <section className={css.section}>
@@ -58,11 +82,11 @@ export default function Todo() {
     <section className={css.sectionTo}>
 
 
-    <AddTodo onClick={takeName}/> 
+    <AddTodo onClick={addName}/> 
     </section>
     <section className={css.sectionMyTodo}>
     <Fillter value={filter} onChange={changeInput} />
-    <MyTodo value={onChangeInput} onTakeBoole={chanheBoole}/>
+    <MyTodo value={filteredToDoes} onTakeBoole={chanheBoole}/>
     
     </section>
     </div>
